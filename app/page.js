65a03ee44b318b0680df2a3dc7279af30d7c0aab
pankajsharma81 <pankajsharma88279@gmail.com>
2025-09-02 08:12:49 +0530

    basic structure of App Layout

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
+      <p className="text-lg">Learning NextJs</p>
       <Button variant="destructive">Click me</Button>
     </div>
   )
