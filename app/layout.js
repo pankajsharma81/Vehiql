7bc10ed6a353ebb2f205a559bc47201a8cee8601
pankajsharma81 <pankajsharma88279@gmail.com>
2025-10-03 00:09:54 +0530

    drag and drop search UI

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/header";
-
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +16,9 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
         <body className={`${inter.className}`}>
-
-          <Header/>
+          <Header />
           <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
           <footer className="bg-blue-50 py-14">
             <div className="container mx-auto px-4 text-center text-gray-600">
               <p>Made By Pankaj Sharma</p>
